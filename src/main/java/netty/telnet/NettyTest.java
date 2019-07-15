255bbb46a6af4944cfa31625499ea03f77f9d19e
cai <1990frog@gmail.com>
2019-07-15 10:57:23 +0800

    edit

---
+++ src/main/java/netty/telnet/NettyTest.java
@@ -0,0 +1,16 @@
+package netty.telnet;
+
+import org.junit.Test;
+
+public class NettyTest {
+
+    @Test
+    public void test() {
+        NettyTelnetServer nettyTelnetServer = new NettyTelnetServer();
+        try {
+            nettyTelnetServer.open();
+        } catch (InterruptedException e) {
+            nettyTelnetServer.close();
+        }
+    }
+}
