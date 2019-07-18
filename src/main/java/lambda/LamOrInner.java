2b77147bc3d304fef88d210c9fc5126ba64b2b66
cai <1990frog@gmail.com>
2019-07-18 16:32:09 +0800

    edit

--- src/main/java/lambda/LamOrInner.java
+++ src/main/java/lambda/LamOrInner.java
@@ -3,12 +3,15 @@ package lambda;
 /**
  *  Lambda 与 内部类
  *
+ *  函数式接口(Functional Interface)：
+ *  就是一个有且仅有一个抽象方法，但是可以有多个非抽象方法的接口。
+ *
  */
 public class LamOrInner {
 
     Runnable r1 = () -> System.out.println("Hello World 1");
 
-    Runnable r2 = new Runnable() {
+    Runnable r2 = new Runnable() {//Functional Interface
         @Override
         public void run() {
             System.out.println("Hello World 2");
