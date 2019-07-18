41f23ed93ecfb2b98a2b2b5c3743160897e02bd9
cai <1990frog@gmail.com>
2019-07-18 17:28:11 +0800

    edit

--- src/main/java/netty/telnet/NettyTelnetHandler.java
+++ src/main/java/netty/telnet/NettyTelnetHandler.java
@@ -10,12 +10,14 @@ import java.util.Date;
 
 public class NettyTelnetHandler extends SimpleChannelInboundHandler<String> {
 
+    @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception{
         ctx.write("Welcome to " + InetAddress.getLocalHost().getHostName() + "!\r\n");
         ctx.write("It is " + new Date() + " now.\r\n");
         ctx.flush();
     }
 
+    @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         cause.printStackTrace();
         ctx.close();
