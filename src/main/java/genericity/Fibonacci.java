035a418e902e38d19f95ca763c4661ce692735f5
cai <1990frog@gmail.com>
2019-08-19 15:57:38 +0800

    commit

--- src/main/java/genericity/Fibonacci.java
+++ src/main/java/genericity/Fibonacci.java
@@ -1,5 +1,10 @@
 package genericity;
 
+/**
+ * 这里注意到，虽然在Fibonacci里面使用的都是int类型，但是其参数类型确是Integer。
+ * 这是因为Java泛型的局限性：基本类型无法作为类型参数。
+ * 但是Java SE5提供了自动包装和拆包的功能，可以很方便地在基本类型和其相应的包装器类型之间进行转换。
+ */
 public class Fibonacci implements Generator<Integer> {
 
     private int count = 0;
