f4b2763e87b497fca9abdc0a0a3239b01b34b60f
1990frog <1990frog@gmail.com>
2019-09-01 23:15:41 +0800

    commit

--- java8/src/main/java/src/main/java/concurrency/moocwukong/threadcoreknowledge/startthread/StartAndRunMethod.java
+++ java8/src/main/java/src/main/java/concurrency/moocwukong/threadcoreknowledge/startthread/StartAndRunMethod.java
@@ -6,10 +6,11 @@ package src.main.java.concurrency.moocwukong.threadcoreknowledge.startthread;
 public class StartAndRunMethod {
 
     public static void main(String[] args) {
+
         Runnable runnable = () -> {
             System.out.println(Thread.currentThread().getName());
-
         };
+
         runnable.run();
 
         new Thread(runnable).start();
