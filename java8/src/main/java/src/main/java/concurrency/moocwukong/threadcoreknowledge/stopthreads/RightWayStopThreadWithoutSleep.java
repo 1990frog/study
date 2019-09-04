7633a8eca9612610c1d2ed68848631b546091e82
cai <1990frog@gmail.com>
2019-09-04 19:31:05 +0800

    commit

--- java8/src/main/java/src/main/java/concurrency/moocwukong/threadcoreknowledge/stopthreads/RightWayStopThreadWithoutSleep.java
+++ java8/src/main/java/src/main/java/concurrency/moocwukong/threadcoreknowledge/stopthreads/RightWayStopThreadWithoutSleep.java
@@ -1,7 +1,15 @@
 package src.main.java.concurrency.moocwukong.threadcoreknowledge.stopthreads;
 
 /**
- * 描述：     run方法内没有sleep或wait方法时，停止线程
+ * 描述：
+ * run方法内没有sleep或wait方法时，停止线程
+ *
+ * 疑问：
+ * 为何run方法中的循环没办法被中断？
+ *
+ * 因为调用Thread.interrupt()方法，只是将线程的状态改变为中断，并不会直接中断线程
+ * 所以可以在循环中判断中断状态是否改变
+ * ?interrupt的中断处理，需要自己手动处理?
  */
 public class RightWayStopThreadWithoutSleep implements Runnable {
 
@@ -14,13 +22,23 @@ public class RightWayStopThreadWithoutSleep implements Runnable {
             }
             num++;
         }
+
+//        /**
+//         * for 也可以实现
+//         */
+//        for(int i=0;i<Integer.MAX_VALUE/2 && !Thread.currentThread().isInterrupted();i++){
+//            if (i % 10000 == 0) {
+//                System.out.println(i + "是10000的倍数");
+//            }
+//        }
+
         System.out.println("任务运行结束了");
     }
 
     public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(new RightWayStopThreadWithoutSleep());
         thread.start();
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         thread.interrupt();
     }
 }
