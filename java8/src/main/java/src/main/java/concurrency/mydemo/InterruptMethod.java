a330f6bdc62dee07a037c30dc5f32ae682a2c5a7
cai <1990frog@gmail.com>
2019-10-18 17:17:42 +0800

    commit

---
+++ java8/src/main/java/src/main/java/concurrency/mydemo/InterruptMethod.java
@@ -0,0 +1,48 @@
+package src.main.java.concurrency.mydemo;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 1.先写个可响应中断的方法
+ * 什么是可响应中断的方法呢？
+ * 那就是有判断当先线程状态的
+ *
+ */
+public class InterruptMethod {
+
+    public void method1() throws InterruptedException{
+        /**
+         * throws InterruptedException
+         * 发生阻塞？
+         * 长时间运行的方法不等于阻塞
+         * 但这两种都推荐有终止运行的机制
+         */
+//        for (;;){
+//            if(Thread.currentThread().isInterrupted()){
+//                break;
+//            }
+//        }
+        while (Thread.currentThread().isInterrupted());
+        throw new InterruptedException();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            InterruptMethod interruptMethod = new InterruptMethod();
+            try {
+                interruptMethod.method1();
+            } catch (InterruptedException e) {
+                System.out.println("this thread is stop!");
+            }
+        });
+        thread.start();
+        Thread.sleep(10);
+        TimeUnit.MILLISECONDS.sleep(100);
+        System.out.println("我要触发interrupt了哈！");
+        thread.interrupt();
+        System.out.println("我触发结束了！");
+        thread.join();
+        System.out.println("thread 线程结束了！");
+    }
+
+}
