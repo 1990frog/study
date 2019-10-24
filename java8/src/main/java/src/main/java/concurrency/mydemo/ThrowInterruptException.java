98fe76db39c3d0400597fc2ab19135a61328566b
cai <1990frog@gmail.com>
2019-10-24 16:31:31 +0800

    commit

---
+++ java8/src/main/java/src/main/java/concurrency/mydemo/ThrowInterruptException.java
@@ -0,0 +1,43 @@
+package src.main.java.concurrency.mydemo;
+
+public class ThrowInterruptException {
+
+    public void interrupt1() throws InterruptedException{
+        while (true);
+    }
+
+    public void interrupt2() throws InterruptedException{
+        while (true){
+            if(Thread.currentThread().isInterrupted()){
+                throw new InterruptedException();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Thread thread1 = new Thread(() -> {
+            try {
+                new ThrowInterruptException().interrupt1();
+            } catch (InterruptedException e) {
+                System.out.println("thread1 is stop!");
+                e.printStackTrace();
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                new ThrowInterruptException().interrupt2();
+            } catch (InterruptedException e) {
+                System.out.println("thread2 is stop!");
+                e.printStackTrace();
+            }
+        });
+
+        thread1.start();
+        thread1.interrupt();
+
+        thread2.start();
+        thread2.interrupt();
+    }
+}
