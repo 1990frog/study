e47bae99ab4f6be6b5d482f5c1a1c20462050661
1990frog <1990frog@gmail.com>
2019-11-04 20:44:54 +0800

    commit

---
+++ dataStructure/src/main/java/arrays/AddElementInArray/Main.java
@@ -0,0 +1,8 @@
+package arrays.AddElementInArray;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ dataStructure/src/main/java/arrays/CreateOurOwnArray/Main.java
@@ -0,0 +1,8 @@
+package arrays.CreateOurOwnArray;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
