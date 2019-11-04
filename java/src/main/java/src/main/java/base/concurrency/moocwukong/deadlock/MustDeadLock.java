b6800371436838a96f1e4f79385cc40bddeb2300
1990frog <1990frog@gmail.com>
2019-11-04 23:37:43 +0800

    commit

--- java/src/main/java/src/main/java/base/concurrency/moocwukong/deadlock/MustDeadLock.java
+++ java/src/main/java/src/main/java/base/concurrency/moocwukong/deadlock/MustDeadLock.java
@@ -1,7 +1,18 @@
 package src.main.java.base.concurrency.moocwukong.deadlock;
 
 /**
- * 描述：     必定发生死锁的情况
+ * 描述：
+ * 必定发生死锁的情况
+ *
+ * 当类的对象flag=1时（T1），先锁定O1，睡眠500毫秒，然后锁定O2；
+ *
+ * 而T1在睡眠的时候另一个flag=0的对象（T2）线程启动，先锁定O2，睡眠500毫秒，等待T1释放O1；
+ *
+ * T1睡眠结束后需要2锁定O2才能继续执行，而此时O2已被T2锁定；
+ *
+ * T2睡眠结束后需要锁定O1才能继续执行，而此时O1已被T1锁定；
+ *
+ * T1、T2相互等待，都需要对方锁定的资源才能继续执行，从而死锁。
  */
 public class MustDeadLock implements Runnable {
 
