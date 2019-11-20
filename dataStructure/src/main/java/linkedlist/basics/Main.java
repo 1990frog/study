d4d8c78f4e5d19ef4284c6cf89e3ede7c587f088
cai <1990frog@gmail.com>
2019-11-20 22:59:13 +0800

    commit

---
+++ dataStructure/src/main/java/linkedlist/basics/Main.java
@@ -0,0 +1,8 @@
+package linkedlist.basics;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ dataStructure/src/main/java/linkedlist/dummyhead/Main.java
@@ -0,0 +1,8 @@
+package linkedlist.dummyhead;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
