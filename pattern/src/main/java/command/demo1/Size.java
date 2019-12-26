694e937346659b999980821eb10a212136a4969c
cai <1990frog@gmail.com>
2019-12-26 11:20:20 +0800

    commit

---
+++ pattern/src/main/java/command/demo1/Size.java
@@ -0,0 +1,22 @@
+package command.demo1;
+
+/**
+ *
+ * Enumeration for target size.
+ *
+ */
+public enum Size {
+
+  SMALL("small"), NORMAL("normal"), LARGE("large"), UNDEFINED("");
+
+  private String title;
+
+  Size(String title) {
+    this.title = title;
+  }
+
+  @Override
+  public String toString() {
+    return title;
+  }
+}
