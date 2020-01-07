51dbce3e4751512d4dc7755890aa4121db91b3dd
cai <1990frog@gmail.com>
2020-01-07 17:08:26 +0800

    commit

---
+++ java/src/main/java/base/datatypes/BaseArrays.java
@@ -0,0 +1,9 @@
+package base.datatypes;
+
+public class BaseArrays {
+
+    public static void main(String[] args) {
+        int[] ints = {1,2,3};
+        Integer[] integers = new Integer[100];
+    }
+}
