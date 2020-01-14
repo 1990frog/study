dca2eda2ec86d9b605cc7e4ef3d553548bfc0540
cai <1990frog@gmail.com>
2020-01-14 15:21:26 +0800

    commit

---
+++ java/src/main/java/base/concurrency/mylock/SynchronizedSuper.java
@@ -0,0 +1,28 @@
+package base.concurrency.mylock;
+
+public class SynchronizedSuper {
+
+    public class A{
+        public synchronized void method1(){
+            System.out.println("this is method1");
+        }
+    }
+
+    public class B extends A{
+        public synchronized void method1(){
+            System.out.println("this is method2");
+            super.method1();
+        }
+    }
+
+    public void test(){
+        B b = new B();
+        b.method1();
+    }
+
+
+    public static void main(String[] args) {
+        SynchronizedSuper synchronizedSuper = new SynchronizedSuper();
+        synchronizedSuper.test();
+    }
+}
