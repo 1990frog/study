daa3effe4fd982ca86a57d06977dfcd390f5d005
cai <1990frog@gmail.com>
2020-01-16 16:29:49 +0800

    commit

---
+++ LeetCode/src/main/java/Solution5.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+import java.util.LinkedList;
+
+/**
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ * 注：回文正反对称
+ *
+ * 示例 1：
+ * 输入: "babad"
+ * 输出: "bab"
+ * 注意: "aba" 也是一个有效答案。
+ *
+ * 示例 2：
+ * 输入: "cbbd"
+ * 输出: "bb"
+ */
+public class Solution5 {
+
+
+    public String longestPalindrome(String s) {
+        LinkedList linkedList = new LinkedList();
+        int head = 0,tail = 0;
+        String ret = "";
+        while (tail < s.length()) {
+
+        }
+        return ret;
+    }
+
+    @Test
+    public void test(){
+        System.out.println(longestPalindrome("babada"));
+        System.out.println(longestPalindrome("adabab"));
+    }
+}
