a4809e11686ead782673537f4882d0df796db6a7
1990frog <1990frog@gmail.com>
2020-01-28 11:28:02 +0800

    commit

--- datastructure/src/main/java/mydatastructure/bst/BST.java
+++ datastructure/src/main/java/mydatastructure/bst/BST.java
@@ -68,6 +68,45 @@ public class BST<E extends Comparable<E>> {
         return false;
     }
 
+    public void preTraverse(){
+        this.preTraverse(root);
+    }
+
+    private void preTraverse(Node node){
+        System.out.println(node.e);
+        if(node.left!=null)
+            preTraverse(node.left);
+        if(node.right!=null)
+            preTraverse(node.right);
+    }
+
+    public void midTraverse(){
+        this.midTraverse(root);
+    }
+
+    private void midTraverse(Node node){
+        if(node.left!=null)
+            midTraverse(node.left);
+
+        System.out.println(node.e);
+
+        if(node.right!=null)
+            midTraverse(node.right);
+
+    }
+
+    public void proTraverse(){
+        this.proTraverse(root);
+    }
+
+    private void proTraverse(Node node){
+        if(node.right!=null)
+            proTraverse(node.right);
+        System.out.println(node.e);
+        if(node.left!=null)
+            proTraverse(node.left);
+    }
+
 
 
     public static void main(String[] args) {
@@ -76,6 +115,9 @@ public class BST<E extends Comparable<E>> {
         bst.add(1);
         bst.add(3);
         bst.add(4);
-        System.out.println(bst.contain(3));
+//        System.out.println(bst.contain(3));
+//        bst.preTraverse();
+//        bst.midTraverse();
+        bst.proTraverse();
     }
 }
