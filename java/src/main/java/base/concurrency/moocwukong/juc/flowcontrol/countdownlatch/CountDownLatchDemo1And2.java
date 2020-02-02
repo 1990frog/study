93bfba7297fa18bf3802d6a883624a0f56aca3ce
cai <1990frog@gmail.com>
2020-02-02 14:07:07 +0800

    commit

--- java/src/main/java/base/concurrency/moocwukong/juc/flowcontrol/countdownlatch/CountDownLatchDemo1And2.java
+++ java/src/main/java/base/concurrency/moocwukong/juc/flowcontrol/countdownlatch/CountDownLatchDemo1And2.java
@@ -5,7 +5,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * 描述：     模拟100米跑步，5名选手都准备好了，只等裁判员一声令下，所有人同时开始跑步。当所有人都到终点后，比赛结束。
+ * 描述：
+ * 模拟100米跑步，5名选手都准备好了，只等裁判员一声令下，所有人同时开始跑步。当所有人都到终点后，比赛结束。
  */
 public class CountDownLatchDemo1And2 {
 
@@ -16,20 +17,17 @@ public class CountDownLatchDemo1And2 {
         ExecutorService service = Executors.newFixedThreadPool(5);
         for (int i = 0; i < 5; i++) {
             final int no = i + 1;
-            Runnable runnable = new Runnable() {
-                @Override
-                public void run() {
-                    System.out.println("No." + no + "准备完毕，等待发令枪");
-                    try {
-                        begin.await();
-                        System.out.println("No." + no + "开始跑步了");
-                        Thread.sleep((long) (Math.random() * 10000));
-                        System.out.println("No." + no + "跑到终点了");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } finally {
-                        end.countDown();
-                    }
+            Runnable runnable = () -> {
+                System.out.println("No." + no + "准备完毕，等待发令枪");
+                try {
+                    begin.await();
+                    System.out.println("No." + no + "开始跑步了");
+                    Thread.sleep((long) (Math.random() * 10000));
+                    System.out.println("No." + no + "跑到终点了");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    end.countDown();
                 }
             };
             service.submit(runnable);
--- java/src/main/java/base/concurrency/moocwukong/juc/flowcontrol/countdownlatch/CountDownLatchDemo2.java
+++ java/src/main/java/base/concurrency/moocwukong/juc/flowcontrol/countdownlatch/CountDownLatchDemo2.java
@@ -5,7 +5,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * 描述：     模拟100米跑步，5名选手都准备好了，只等裁判员一声令下，所有人同时开始跑步。
+ * 描述：
+ * 模拟100米跑步，5名选手都准备好了，只等裁判员一声令下，所有人同时开始跑步。
  */
 public class CountDownLatchDemo2 {
 
@@ -14,16 +15,13 @@ public class CountDownLatchDemo2 {
         ExecutorService service = Executors.newFixedThreadPool(5);
         for (int i = 0; i < 5; i++) {
             final int no = i + 1;
-            Runnable runnable = new Runnable() {
-                @Override
-                public void run() {
-                    System.out.println("No." + no + "准备完毕，等待发令枪");
-                    try {
-                        begin.await();
-                        System.out.println("No." + no + "开始跑步了");
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+            Runnable runnable = () -> {
+                System.out.println("No." + no + "准备完毕，等待发令枪");
+                try {
+                    begin.await();
+                    System.out.println("No." + no + "开始跑步了");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             };
             service.submit(runnable);
