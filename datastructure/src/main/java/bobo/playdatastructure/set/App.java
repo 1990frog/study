f6dfc4b025f141d092d046164d298915728ee42f
cai <1990frog@gmail.com>
2020-02-07 22:37:26 +0800

    commit

--- datastructure/src/main/java/bobo/playdatastructure/set/App.java
+++ datastructure/src/main/java/bobo/playdatastructure/set/App.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 
 public class App {
 
+    private static final String PRIDE_AND_PREJUDICE = "/home/cai/Code/Java/study/datastructure/src/main/java/bobo/playdatastructure/set/resource/pride-and-prejudice.txt";
+    private static final String A_TALE_OF_TWO_CITIES = "/home/cai/Code/Java/study/datastructure/src/main/java/bobo/playdatastructure/set/resource/a-tale-of-two-cities.txt";
     public static void main(String[] args) {
 
         System.out.println("Pride and Prejudice");
 
         ArrayList<String> words1 = new ArrayList<>();
-        if(FileOperation.readFile("/home/cai/Code/Java/study/datastructure/src/main/java/bobo/playdatastructure/set/linkedListSet/pride-and-prejudice.txt", words1)) {
+        if(FileOperation.readFile(PRIDE_AND_PREJUDICE, words1)) {
             System.out.println("Total words: " + words1.size());
 
             LinkedListSet<String> set1 = new LinkedListSet<>();
@@ -24,7 +26,7 @@ public class App {
         System.out.println("A Tale of Two Cities");
 
         ArrayList<String> words2 = new ArrayList<>();
-        if(FileOperation.readFile("/home/cai/Code/Java/study/datastructure/src/main/java/bobo/playdatastructure/set/linkedListSet/a-tale-of-two-cities.txt", words2)){
+        if(FileOperation.readFile(A_TALE_OF_TWO_CITIES, words2)){
             System.out.println("Total words: " + words2.size());
 
             LinkedListSet<String> set2 = new LinkedListSet<>();
