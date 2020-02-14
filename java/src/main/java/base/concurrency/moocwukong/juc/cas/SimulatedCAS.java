430576208f8029aa4b570db94359b31bb38289ae
cai <1990frog@gmail.com>
2020-02-14 11:25:35 +0800

    commit

--- java/src/main/java/base/concurrency/moocwukong/juc/cas/SimulatedCAS.java
+++ java/src/main/java/base/concurrency/moocwukong/juc/cas/SimulatedCAS.java
@@ -1,10 +1,15 @@
 package base.concurrency.moocwukong.juc.cas;
 
 /**
- * 描述：     模拟CAS操作，等价代码
+ * 描述：
+ * 模拟CAS操作，等价代码
  */
 public class SimulatedCAS {
-    private volatile int value;
+    private volatile int value = 1;
+
+    public synchronized int get(){
+        return value;
+    }
 
     public synchronized int compareAndSwap(int expectedValue, int newValue) {
         int oldValue = value;
