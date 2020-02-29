9789bb46ef243e397409f5eb796a7375b23ad74c
cai <1990frog@gmail.com>
2020-02-29 17:30:35 +0800

    commit

--- springboot/src/main/java/com/SpringbootApplication.java
+++ springboot/src/main/java/com/SpringbootApplication.java
@@ -3,7 +3,9 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import tk.mybatis.spring.annotation.MapperScan;
 
+@MapperScan("com.mvc")
 @SpringBootApplication
 //@PropertySource({"demo.properties"})
 public class SpringbootApplication {
