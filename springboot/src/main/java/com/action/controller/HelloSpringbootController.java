a56fa6e32e33153c681b29c0a56e04f8e1406876
cai <1990frog@gmail.com>
2020-03-01 21:00:36 +0800

    commit

--- springboot/src/main/java/com/action/controller/HelloSpringbootController.java
+++ springboot/src/main/java/com/action/controller/HelloSpringbootController.java
@@ -1,7 +1,24 @@
 package com.action.controller;
 
+import com.action.except.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class HelloSpringbootController {
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String demo(){
+//        log.info("info");
+//        log.warn("warn");
+//        log.error("error");
+//        MDC.put("KEY","VALUE");
+        throw new BusinessException("haha");
+//        return "hello logback";
+    }
 }
