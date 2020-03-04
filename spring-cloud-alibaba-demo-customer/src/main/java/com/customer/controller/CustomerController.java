34de88eb24cbad44b19089489283bc2c32ef831a
cai <1990frog@gmail.com>
2020-03-05 01:05:24 +0800

    commit

--- spring-cloud-alibaba-demo-customer/src/main/java/com/customer/controller/CustomerController.java
+++ spring-cloud-alibaba-demo-customer/src/main/java/com/customer/controller/CustomerController.java
@@ -1,11 +1,22 @@
 package com.customer.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 public class CustomerController {
 
-//    public String customer(){
-//
-//    }
+//    @NacosInjected
+//    private ConfigService configService;
+
+    @GetMapping("/customer")
+    @ResponseBody
+    public String customer(){
+//        log.info(configService.getServerStatus());
+//        return configService.getServerStatus();
+        return "success";
+    }
 }
