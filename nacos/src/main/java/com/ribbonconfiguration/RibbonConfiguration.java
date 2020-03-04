672cbc65f8a8e931813e8274fea8b1a6b0d1fd70
cai <1990frog@gmail.com>
2020-03-04 17:34:20 +0800

    commit

--- nacos/src/main/java/com/ribbonconfiguration/RibbonConfiguration.java
+++ nacos/src/main/java/com/ribbonconfiguration/RibbonConfiguration.java
@@ -1,6 +1,9 @@
 package com.ribbonconfiguration;
 
+import com.nacos.configuration.NacosWeightedRule;
+import com.netflix.loadbalancer.IPing;
 import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.PingUrl;
 import com.netflix.loadbalancer.RandomRule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +13,13 @@ public class RibbonConfiguration {
 
     @Bean
     public IRule ribbonRule(){
-        return new RandomRule();
+//        return new RandomRule();
+        // 自定义rule
+        return new NacosWeightedRule();
+    }
+
+    @Bean
+    public IPing ping(){
+        return new PingUrl();
     }
 }
