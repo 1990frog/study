0a455019f308e3b0569132c472d643915fb1f7c9
cai <1990frog@gmail.com>
2020-03-06 21:54:43 +0800

    commit

--- springboot-base/src/main/java/com/config/RedisConfiguration.java
+++ springboot-base/src/main/java/com/config/RedisConfiguration.java
@@ -1,5 +1,6 @@
 package com.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -8,12 +9,18 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfiguration {
 
+    @Bean
     public RedisTemplate<String,Object> redisTemplate(LettuceConnectionFactory factory){
         RedisTemplate<String,Object> redisTemplate = new RedisTemplate<>();
+        // key序列化方式
         redisTemplate.setKeySerializer(new StringRedisSerializer());
+        // value序列化方式
         redisTemplate.setValueSerializer(new StringRedisSerializer());
+        // hashkey序列化方式
         redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        // hashvalue序列化方式
         redisTemplate.setHashValueSerializer(new StringRedisSerializer());
+        // 注入连接工厂
         redisTemplate.setConnectionFactory(factory);
         return redisTemplate;
     }
