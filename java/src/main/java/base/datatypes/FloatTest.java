9bc38b3d4ea2797c23596823df0ab0023131f88b
cai <1990frog@gmail.com>
2020-03-07 21:25:51 +0800

    commit

---
+++ java/src/main/java/base/datatypes/FloatTest.java
@@ -0,0 +1,32 @@
+package base.datatypes;
+
+public class FloatTest {
+
+    /**
+     * 0.060000000000000005
+     * 0.5800000000000001
+     * 401.49999999999994
+     * 1.2329999999999999
+     * 4.01
+     */
+    public static void main(String[] args) {
+        // 0.060000000000000005
+        System.out.println(0.05+0.01);
+        // 0.5800000000000001
+        System.out.println(1.0-0.42);
+        // 401.49999999999994
+        System.out.println(4.015*100);
+        // 1.2329999999999999
+        System.out.println(123.3/100);
+        // 4.01 四舍五入保留两位
+        System.out.println(Math.round(4.015*100)/100.0);
+
+        double a = 0.05;
+        double b = 0.01;
+        System.out.println(a+b);
+
+        Double c = 0.05;
+        Double d = 0.01;
+        System.out.println(c+d);
+    }
+}
