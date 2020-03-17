a91c1922f10eb38eed5bf3485c382deff8e7cbe0
cai <1990frog@gmail.com>
2020-03-18 05:03:43 +0800

    commit

--- springcloud-alibaba-demo-customer/src/main/java/com/customer/CustomerApplication.java
+++ springcloud-alibaba-demo-customer/src/main/java/com/customer/CustomerApplication.java
@@ -1,10 +1,14 @@
 package com.customer;
 
+import com.alibaba.cloud.sentinel.annotation.SentinelRestTemplate;
 import com.customer.configuration.ProductorFeignConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 //@EnableFeignClients(defaultConfiguration = ProductorFeignConfiguration.class)
@@ -13,6 +17,13 @@ public class CustomerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    @SentinelRestTemplate
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 }
