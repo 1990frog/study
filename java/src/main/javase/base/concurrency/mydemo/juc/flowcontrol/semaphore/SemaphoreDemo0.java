365b115fbdbdcdefac99ae78c2110fdad72683ee
cai <1990frog@gmail.com>
2020-04-23 01:21:26 +0800

    commit

--- java/src/main/javase/base/concurrency/mydemo/juc/flowcontrol/semaphore/SemaphoreDemo0.java
+++ java/src/main/javase/base/concurrency/mydemo/juc/flowcontrol/semaphore/SemaphoreDemo0.java
@@ -6,9 +6,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 
-public class SemaphoreDemo implements Runnable {
+public class SemaphoreDemo0 implements Runnable {
 
     static Semaphore semaphore = new Semaphore(1,true);
 //    static AtomicInteger counter = new AtomicInteger(0);
@@ -27,7 +26,7 @@ public class SemaphoreDemo implements Runnable {
 
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(100);
-        SemaphoreDemo runner = new SemaphoreDemo();
+        SemaphoreDemo0 runner = new SemaphoreDemo0();
         for (int i = 0; i < 10; i++) {
             executorService.submit(runner);
         }
