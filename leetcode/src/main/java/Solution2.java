9f6da8bed79847c5fe17851599ddbcdaad740062
caijingquan <caijingquan@clinbrain.com>
2022-05-07 10:07:05 +0800

    commit

--- leetcode/src/main/java/Solution2.java
+++ leetcode/src/main/java/Solution2.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.NodeList;
+
 public class Solution2 {
 
     private class ListNode {
@@ -18,41 +20,35 @@ public class Solution2 {
 
     }
 
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        return add(l1, l2, 0);
+//    }
+//
+//    private ListNode add(ListNode l1, ListNode l2, int value) {
+//
+//        if (l1 == null && l2 == null)
+//            if (value > 0)
+//                return new ListNode(value);
+//            else
+//                return null;
+//
+//        int val = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + value;
+//        ListNode node = new ListNode(val % 10);
+//        node.next = add(l1 == null ? null : l1.next, l2 == null ? null : l2.next, val / 10);
+//        return node;
+//    }
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return add(l1, l2, 0);
+        ListNode l1Cur = l1;
+        ListNode l2Cur = l2;
+        while (l1Cur!=null || l2Cur!=null){
+
+        }
+        return l1;
     }
 
-    private ListNode add(ListNode l1, ListNode l2, int value) {
+    private void addLevel(ListNode ln){
 
-        int val1 = 0;
-        int val2 = 0;
-        ListNode l1next = null;
-        ListNode l2next = null;
-
-        if (l1 == null && l2 == null)
-            if (value > 0)
-                return new ListNode(value);
-            else
-                return null;
-
-        if (l1 != null) {
-            val1 = l1.val;
-            l1next = l1.next;
-        }
-        if (l2 != null) {
-            val2 = l2.val;
-            l2next = l2.next;
-        }
-
-
-        int val = val1 + val2 + value;
-        ListNode node = new ListNode(val % 10);
-        if (val > 9) {
-            node.next = add(l1next, l2next, 1);
-        } else {
-            node.next = add(l1next, l2next, 0);
-        }
-        return node;
     }
 
 }
