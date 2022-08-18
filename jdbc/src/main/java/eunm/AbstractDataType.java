0d1f97f723c68fa4e3cf3f4d278e93fd1d707625
caijingquan <caijingquan@clinbrain.com>
2022-08-18 12:56:56 +0800

    Changes

---
+++ jdbc/src/main/java/eunm/AbstractDataType.java
@@ -0,0 +1,18 @@
+package eunm;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * <p>
+ * 抽象数据库字段类型
+ * </p>
+ *
+ * @author caijingquan@clinbrain.com
+ * @since 2022/8/18
+ */
+@Getter
+@AllArgsConstructor
+public enum AbstractDataType {
+    STRING, NUMERIC, DATE;
+}
---
+++ jdbc/src/main/java/eunm/DBMS.java
@@ -0,0 +1,24 @@
+package eunm;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum DBMS {
+
+//    mysql("com.mysql.cj.jdbc.Driver", "jdbc:mysql://%s:%s/%s?serverTimezone=UTC"),
+//    oracle("oracle.jdbc.driver.OracleDriver", "jdbc:oracle:thin:@%s:%s:%s"),
+//    sqlserver("com.microsoft.sqlserver.jdbc.SQLServerDriver", "jdbc:sqlserver://%s:%s;database=%s"),
+//    greenplum("org.postgresql.Driver", "jdbc:postgresql://%s:%s/%s"),
+//    hive("org.apache.hive.jdbc.HiveDriver", "jdbc:hive2://%s:%s/%s"),
+//    presto("com.facebook.presto.jdbc.PrestoDriver", "jdbc:presto://%s:%s/%s"),
+//    sybase("net.sourceforge.jtds.jdbc.Driver", "jdbc:jtds:sybase://%s:%s/%s"),
+//    cache("com.intersys.jdbc.CacheDriver", "jdbc:Cache://%s:%s/%s");
+//
+//    private final String driver;
+//    private final String jdbcUrl;
+//    private final String queryTablesSql;
+//    private final String queryColumnsSql;
+
+}
