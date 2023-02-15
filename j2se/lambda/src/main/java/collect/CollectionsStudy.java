f89cfbb535fb12f445464da795b2f3200facdbbd
1990frog <1990frog@gmail.com>
2023-02-15 17:50:00 +0800

    commit

--- j2se/lambda/src/main/java/collect/CollectionsStudy.java
+++ j2se/lambda/src/main/java/collect/CollectionsStudy.java
@@ -1,43 +1,32 @@
 package collect;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import org.testng.annotations.Test;
+import util.User;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * <p>
  * 收集器
- * <R, A> R collect(Collector<? super T, A, R> collector)
+ *
+ * <p>
+ * <R> R collect(Supplier<R> var1, BiConsumer<R, ? super T> var2, BiConsumer<R, R> var3);
+ * <R, A> R collect(Collector<? super T, A, R> var1);
  * </p>
  *
  * @author cai
  * @since 2023/2/14
  */
-public class Test {
+public class CollectionsStudy {
 
-    @Data
-    @Builder
-    @AllArgsConstructor
-    static class User {
-        private int gender;
-        private int age;
-        private String name;
-    }
-
-    private static final List<User> data = new ArrayList<User>() {{
-        add(User.builder().name("lilei").age(12).gender(0).build());
-        add(User.builder().name("hanmeimei").age(12).gender(1).build());
-        add(User.builder().name("tom").age(13).gender(0).build());
-    }};
+    private static final List<User> data = User.getData();
 
     /**
      * 工厂方法：toList()
      * 把流中的所有项目收集到一个List
+     *
      */
-    @org.testng.annotations.Test
+    @Test
     public void toList() {
         List<String> list = data.stream().map(User::getName).collect(Collectors.toList());
         System.out.println(list);
@@ -47,7 +36,7 @@ public class Test {
      * 工厂方法：toSet()
      * 把流中的所有项目收集到一个Set，删除重复项
      */
-    @org.testng.annotations.Test
+    @Test
     public void toSet() {
         Set<Integer> set = data.stream().map(User::getAge).collect(Collectors.toSet());
         System.out.println(set);
@@ -57,7 +46,7 @@ public class Test {
      * 工厂方法：toMap()
      * 把流中的所有项目收集到一个Map
      */
-    @org.testng.annotations.Test
+    @Test
     public void toMap() {
         Map<String, User> map = data.stream().collect(Collectors.toMap(User::getName, n -> n));
         System.out.println(map);
@@ -67,13 +56,13 @@ public class Test {
      * 工厂方法：toCollection()
      * 把流中所有项目收集到给定的供应源创建的集合
      */
-    @org.testng.annotations.Test
+    @Test
     public void toCollection() {
         Collection<User> collection = data.stream().collect(Collectors.toCollection(ArrayList::new));
         System.out.println(collection);
     }
 
-    @org.testng.annotations.Test
+    @Test
     public void counting() {
         long count1 = data.stream().collect(Collectors.counting());
         long count2 = data.stream().count();
@@ -82,7 +71,7 @@ public class Test {
 
     }
 
-    @org.testng.annotations.Test
+    @Test
     public void summingInt() {
         int sum1 = data.stream().mapToInt(User::getAge).sum();
         int sum2 = data.stream().collect(Collectors.summingInt(User::getAge));
@@ -90,57 +79,59 @@ public class Test {
         System.out.println(sum2);
     }
 
-    @org.testng.annotations.Test
-    public void averagingInt(){
+    @Test
+    public void averagingInt() {
         Double avg = data.stream().collect(Collectors.averagingInt(User::getAge));
         System.out.println(avg);
     }
 
-    @org.testng.annotations.Test
-    public void summarizingInt(){
+    @Test
+    public void summarizingInt() {
         IntSummaryStatistics summary = data.stream().collect(Collectors.summarizingInt(User::getAge));
         System.out.println(summary);
     }
 
-    @org.testng.annotations.Test
-    public void joining(){
+    @Test
+    public void joining() {
         String joining = data.stream().map(User::getName).collect(Collectors.joining(","));
         System.out.println(joining);
     }
 
-    @org.testng.annotations.Test
-    public void maxBy(){
+    @Test
+    public void maxBy() {
         Optional<User> max = data.stream().collect(Collectors.maxBy(Comparator.comparingInt(User::getAge)));
         System.out.println(max);
     }
 
-    @org.testng.annotations.Test
-    public void minBy(){
+    @Test
+    public void minBy() {
         Optional<User> min = data.stream().collect(Collectors.minBy(Comparator.comparingInt(User::getAge)));
         System.out.println(min);
     }
 
-    @org.testng.annotations.Test
-    public void reducing(){
-        int total = data.stream().collect(Collectors.reducing(0,User::getAge,Integer::sum));
+    @Test
+    public void reducing() {
+        int total = data.stream().collect(Collectors.reducing(0, User::getAge, Integer::sum));
         System.out.println(total);
     }
 
-    @org.testng.annotations.Test
-    public void collectingAndThen(){
-        int pass = data.stream().collect(Collectors.collectingAndThen(Collectors.toList(),List::size));
+    @Test
+    public void collectingAndThen() {
+        int pass = data.stream().collect(Collectors.collectingAndThen(Collectors.toList(), List::size));
         System.out.println(pass);
     }
 
-    @org.testng.annotations.Test
-    public void groupingBy(){
-        Map<String,List<User>> grouping = data.stream().collect(Collectors.groupingBy(User::getName));
+    @Test
+    public void groupingBy() {
+        Map<String, List<User>> grouping = data.stream().collect(Collectors.groupingBy(User::getName));
         System.out.println(grouping);
     }
 
-    @org.testng.annotations.Test
-    public void partitioningBy(){
-        Map<Boolean,List<User>> partitioning = data.stream().collect(Collectors.partitioningBy(n->{return n.gender == 1;}));
+    @Test
+    public void partitioningBy() {
+        Map<Boolean, List<User>> partitioning = data.stream().collect(Collectors.partitioningBy(n -> {
+            return n.getGender() == 1;
+        }));
         System.out.println(partitioning);
     }
 
