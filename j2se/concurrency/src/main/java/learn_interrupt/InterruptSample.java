5e40de96582e8759614cae3be7eb423b2cb9e9ac
cai <1990frog@gmail.com>
2024-11-28 23:15:17 +0800

    commit

--- j2se/concurrency/src/main/java/learn_interrupt/InterruptSample.java
+++ j2se/concurrency/src/main/java/learn_interrupt/InterruptSample.java
@@ -4,21 +4,25 @@ import lombok.SneakyThrows;
 
 import java.util.concurrent.TimeUnit;
 
-public class Demo1 implements Runnable {
+public class InterruptSample implements Runnable {
 
     @Override
     public void run() {
+        // 获取线程的 isInterrupted 状态
         while (!Thread.currentThread().isInterrupted()){
             System.out.println("This thread is not interrupted");
+            // 检测当前的中断状态，并清除
+            System.out.println(Thread.interrupted());
         }
         System.out.println("This thread is interrupted");
     }
 
     @SneakyThrows
     public static void main(String[] args) {
-        Thread thread = new Thread(new Demo1());
+        Thread thread = new Thread(new InterruptSample());
         thread.start();
         TimeUnit.SECONDS.sleep(2);
+        // 将线程 isInterrupted 状态设置为 false
         thread.interrupt();
     }
 }
